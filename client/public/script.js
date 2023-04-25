e3965e6fa02d205c9d51a97db3265a5067f337fb
Jakob <M.Jakob.inf21@stud.bbbaden.ch>
2023-04-25 18:51:07 +0200

    Update

--- client/public/script.js
+++ client/public/script.js
@@ -45,10 +45,12 @@ const observer = new IntersectionObserver((entries) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     } else {
-      entry.target.classList.remove("show");
+      //entry.target.classList.remove("show"); 
     }
   });
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Contact animation
